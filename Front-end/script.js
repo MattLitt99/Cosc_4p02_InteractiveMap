2895d274a36f1fdb48701f6c5075a196c1722ac2
Ngela <ns17uq@brocku.ca>
2023-03-07 01:34:53 -0500

    Changes default exhibit status to Active

--- Front-end/script.js
+++ Front-end/script.js
@@ -3,7 +3,7 @@
 exhibit = 
 { 
     name:"Lives of the Gods: Divinity in Maya Art",
-    status:"1", 
+    status:"0", 
     roomNumber:"303",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 };
