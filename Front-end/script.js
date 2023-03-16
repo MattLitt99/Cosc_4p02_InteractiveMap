643ce320dd319e49e1648e82fe4377c849677bf1
Ngela <ns17uq@brocku.ca>
2023-03-16 02:02:10 -0400

    Added table of exhibits

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,12 +1,58 @@
 // Filler data for the front end 
 exhibit = 
 { 
+    id: 1,
     name:"Lives of the Gods: Divinity in Maya Art",
     status:"0", 
     roomNumber:"303",
     year:"500 B.C",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    keywords:["one", "two", "three"],
+    images:["img1","img2", "img3"]
 };
+
+var exhibits = [];
+exhibits[0] = { 
+    id: 1,
+    name:"Lives of the Gods: Divinity in Maya Art",
+    status:"0", 
+    roomNumber:"303",
+    year:"500 B.C",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    keywords:["one", "two", "three"],
+    images:["img1","img2", "img3"]
+};
+exhibits[1] = { 
+    id: 2,
+    name:"African Religion from 400 A.D.",
+    status:"0", 
+    roomNumber:"107",
+    year:"400 A.D",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    keywords:["one", "two", "three"],
+    images:["img1","img2", "img3"]
+};
+exhibits[2] = { 
+    id: 3,
+    name:"Along the Water Front",
+    status:"0", 
+    roomNumber:"226",
+    year:"1200",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    keywords:["one", "two", "three"],
+    images:["img1","img2", "img3"]
+};
+exhibits[3] = { 
+    id: 4,
+    name:"Google Arts and Culture",
+    status:"0", 
+    roomNumber:"130",
+    year:"500 B.C",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    keywords:["one", "two", "three"],
+    images:["img1","img2", "img3"]
+};
+
 
 // Changes closes the viewing part of page and replaces it with form to edit
 function openEditor(){
@@ -96,4 +142,50 @@ function navSearch(){
     else {
         console.log("Search for "+temp.value);
     }
+}
+
+//Creates table to display exhibits
+function loadExhibitsList(){
+    var table, row;
+    // Each iteration creates one row of the tabele
+     for (let m = 0; m < exhibits.length; m++ ){
+        row = document.createElement("tr");
+        var tds = [];
+        //creates and populates array of td HTML elements
+        for ( var i = 0; i < 5; i++ ) {
+            tds[i] = document.createElement("td");
+        }
+        //So the Name of the exhibit is a link
+        aName = document.createElement("a");
+        aName.innerHTML = exhibits[m].name;
+        aName.href="viewExhibit.html";
+
+        //creates and populates array of the exhibits attributes
+        var data = [];
+        data[0] = document.createTextNode(exhibits[m].id);
+        //skip data[1]
+        data[2] = document.createTextNode(exhibits[m].year);
+        data[3] = document.createTextNode(exhibits[m].status);
+        data[4] = document.createTextNode(exhibits[m].roomNumber);
+
+        // adds each attribute to a td object
+        for ( var i = 0; i < 5; i++ ) {
+            // use link element if it's the name
+            if ( i == 1) {
+                tds[i].appendChild(aName);
+            }
+            else {
+                tds[i].appendChild(data[i]);
+            }
+        }
+        
+        //adds the td objects to one row
+        for ( var i = 0; i < 5; i++ ) {
+            row.appendChild(tds[i]);
+        }
+
+        //adds the row to the table
+        table = document.getElementById("viewExhibitsTable");
+        table.appendChild(row);
+     }
 }
